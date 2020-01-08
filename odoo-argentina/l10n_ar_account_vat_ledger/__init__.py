962345bf075f530fdf286719e385084c7189c6b1
Key <keyla@moogah.com>
2020-01-08 11:52:39 -0300

    added odoo-argentina v13

---
+++ odoo-argentina/l10n_ar_account_vat_ledger/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import account
+from . import account_invoice
+from . import account_vat_report
